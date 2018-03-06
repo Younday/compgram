04a543bc0c0649c16dda2bed63cb09fee76dbb3f
Younes Moustaghfir <younesmous@gmail.com>
2018-03-06 20:16:34 +0100

    c

--- src/tinydepparser.py
+++ src/tinydepparser.py
@@ -366,7 +366,7 @@ def extract_features(words, lemmas, tags, current_position_buffer, n, stack, par
     if s_w1 and b_w1: features.append(('s_w1=%s b_w1=%s' % (s_w1, b_w1), 1))
     if s_w2 and b_w2: features.append(('s_w2=%s b_w2=%s' % (s_w2, b_w2), 1))
 
-    if b_l_w0 and s_l_w0: features.append(('b_l_w0=%s s_l_w0=%s' % (b_l_w0, s_l_w0), 1))
+    #if b_l_w0 and s_l_w0: features.append(('b_l_w0=%s s_l_w0=%s' % (b_l_w0, s_l_w0), 1))
 
 
 
