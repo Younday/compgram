c821133f763632da6af293acbb13b892735f4d97
Younes Moustaghfir <younesmous@gmail.com>
2018-03-06 19:17:39 +0100

    c

--- src/tinydepparser.py
+++ src/tinydepparser.py
@@ -357,9 +357,14 @@ def extract_features(words, lemmas, tags, current_position_buffer, n, stack, par
     # add combination (WHAT DOES THIS DO?)
     if s_w0 and s_p0: features.append(('s_w0=%s s_p0=%s' % (s_w0, s_p0), 1))
     if s_w1 and s_p1: features.append(('s_w1=%s s_p1=%s' % (s_w1, s_p1), 1))
+    if s_w2 and s_p2: features.append(('s_w2=%s s_p2=%s' % (s_w2, s_p2), 1))
 
-    #if b_l_w0 and b_r_w0: features.append(('b_l_w0=%s b_r_w0=%s' % (b_l_w0, b_r_w0), 1))
-    #if b_l_w1 and b_r_w1: features.append(('b_l_w1=%s b_r_w1=%s' % (b_l_w1, b_r_w1), 1))
+    if b_w0 and b_p0: features.append(('s_w0=%s s_p0=%s' % (b_w0, b_p0), 1))
+    if b_w1 and b_p1: features.append(('s_w1=%s s_p1=%s' % (b_w1, b_p1), 1))
+    if b_w2 and b_p2: features.append(('s_w2=%s s_p2=%s' % (b_w2, b_p2), 1))
+
+    if b_l_w0 and b_l_w0: features.append(('b_l_w0=%s b_r_w0=%s' % (b_l_w0, b_r_w0), 1))
+    if b_l_w1 and b_r_w1: features.append(('b_l_w1=%s b_r_w1=%s' % (b_l_w1, b_r_w1), 1))
 
     # TODO: ADD MORE
 
